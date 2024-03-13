3730c9c64078a44ef158a3a46f4e40b640da2305
Lakshmi Sravani <137598500+DataWhizML@users.noreply.github.com>
2024-03-14 01:13:18 +0530

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,25 @@
+import google.generativeai as genai
+import streamlit as st
+import PIL.Image
+from io import BytesIO
+
+model = genai.GenerativeModel('gemini-pro-vision')
+genai.configure(api_key="AIzaSyBFOLzPXNfyT9pAIntAo7-Jeo9VU-0v5Z0")
+
+def image_to_text(image):
+    img = PIL.Image.open(BytesIO(image))
+    response = model.generate_content(img)
+    return response.text.strip()
+
+def main():
+    st.title("Gemini-Pro-Vision")
+    image = st.file_uploader("Upload an Image", type=['jpg','jpeg','png'])
+    if image is not None:
+        st.image(image)
+    if st.button("Get Response"):
+        result = image_to_text(image.read())
+        st.success(result)
+
+if __name__ == '__main__':
+    main()
+
