727048e3e9bb14b82571670a8ad69e97f86d2fae
Lakshmi Sravani <137598500+DataWhizML@users.noreply.github.com>
2024-03-14 01:13:41 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,7 +4,7 @@ import PIL.Image
 from io import BytesIO
 
 model = genai.GenerativeModel('gemini-pro-vision')
-genai.configure(api_key="AIzaSyBFOLzPXNfyT9pAIntAo7-Jeo9VU-0v5Z0")
+genai.configure(api_key="API-KEY")
 
 def image_to_text(image):
     img = PIL.Image.open(BytesIO(image))
